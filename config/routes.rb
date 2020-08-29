a7b7ac54ae49528c46da65b0d2e5c60011ead879
Jhumur Chatterjee <jhumurchatterjee1996@gmail.com>
2020-08-29 20:14:34 +0530

    Changing routes for message index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  resources :users, only: :index
-  resources :messages, only: %i[index create]
+  resources :users, only: :index do
+    resources :messages, only: :index
+  end
+
+  resources :messages, only: :create
 
   post '/signup', to: 'users#create'
   post '/login',  to: 'sessions#create'
